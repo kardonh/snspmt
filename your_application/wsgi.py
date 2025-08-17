8d5948c0c8a91070425bb5ec4b73b466051ac904
kardonh <143279283+kardonh@users.noreply.github.com>
2025-08-17 16:31:19 +0900

    Fix gunicorn application attribute error - Add application attribute to your_application/wsgi.py - Resolve AppImportError in production deployment

--- your_application/wsgi.py
+++ your_application/wsgi.py
@@ -3,6 +3,7 @@ from . import your_application
 
 # gunicorn your_application.wsgi 명령어에 대응
 app = your_application
+application = your_application  # gunicorn이 찾는 속성
 
 if __name__ == "__main__":
     app.run()
